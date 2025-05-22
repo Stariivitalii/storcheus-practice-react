942995e24b4300157a2086a558a0b7cfc9a105bb
Vitalii Storcheus <storcheusvitalii@gmail.com>
2025-05-22 11:05:36 +0300

    make all products

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,26 @@
 import React from 'react';
 import './App.scss';
 
-// import usersFromServer from './api/users';
-// import categoriesFromServer from './api/categories';
-// import productsFromServer from './api/products';
+import usersFromServer from './api/users';
+import categoriesFromServer from './api/categories';
+import productsFromServer from './api/products';
 
-// const products = productsFromServer.map((product) => {
-//   const category = null; // find by product.categoryId
-//   const user = null; // find by category.ownerId
+const products = productsFromServer.map(product => {
+  const category = categoriesFromServer.find(categ => {
+    return categ === product.categoryId;
+  });
+  const user = usersFromServer.find(person => {
+    return person.id === category.ownerId;
+  });
 
-//   return null;
-// });
+  return {
+    ...products,
+    categoryTitle: category.title,
+    categoryIcon: category.icon,
+    userName: user.name,
+    userSex: user.sex,
+  };
+});
 
 export const App = () => (
   <div className="section">
@@ -23,32 +33,19 @@ export const App = () => (
           <p className="panel-heading">Filters</p>
 
           <p className="panel-tabs has-text-weight-bold">
-            <a
-              data-cy="FilterAllUsers"
-              href="#/"
-            >
+            <a data-cy="FilterAllUsers" href="#/">
               All
             </a>
 
-            <a
-              data-cy="FilterUser"
-              href="#/"
-            >
+            <a data-cy="FilterUser" href="#/">
               User 1
             </a>
 
-            <a
-              data-cy="FilterUser"
-              href="#/"
-              className="is-active"
-            >
+            <a data-cy="FilterUser" href="#/" className="is-active">
               User 2
             </a>
 
-            <a
-              data-cy="FilterUser"
-              href="#/"
-            >
+            <a data-cy="FilterUser" href="#/">
               User 3
             </a>
           </p>
@@ -95,11 +92,7 @@ export const App = () => (
               Category 1
             </a>
 
-            <a
-              data-cy="Category"
-              className="button mr-2 my-1"
-              href="#/"
-            >
+            <a data-cy="Category" className="button mr-2 my-1" href="#/">
               Category 2
             </a>
 
@@ -110,11 +103,7 @@ export const App = () => (
             >
               Category 3
             </a>
-            <a
-              data-cy="Category"
-              className="button mr-2 my-1"
-              href="#/"
-            >
+            <a data-cy="Category" className="button mr-2 my-1" href="#/">
               Category 4
             </a>
           </div>
@@ -145,7 +134,6 @@ export const App = () => (
               <th>
                 <span className="is-flex is-flex-wrap-nowrap">
                   ID
-
                   <a href="#/">
                     <span className="icon">
                       <i data-cy="SortIcon" className="fas fa-sort" />
@@ -157,7 +145,6 @@ export const App = () => (
               <th>
                 <span className="is-flex is-flex-wrap-nowrap">
                   Product
-
                   <a href="#/">
                     <span className="icon">
                       <i data-cy="SortIcon" className="fas fa-sort-down" />
@@ -169,7 +156,6 @@ export const App = () => (
               <th>
                 <span className="is-flex is-flex-wrap-nowrap">
                   Category
-
                   <a href="#/">
                     <span className="icon">
                       <i data-cy="SortIcon" className="fas fa-sort-up" />
@@ -181,7 +167,6 @@ export const App = () => (
               <th>
                 <span className="is-flex is-flex-wrap-nowrap">
                   User
-
                   <a href="#/">
                     <span className="icon">
                       <i data-cy="SortIcon" className="fas fa-sort" />
@@ -201,10 +186,7 @@ export const App = () => (
               <td data-cy="ProductName">Milk</td>
               <td data-cy="ProductCategory">🍺 - Drinks</td>
 
-              <td
-                data-cy="ProductUser"
-                className="has-text-link"
-              >
+              <td data-cy="ProductUser" className="has-text-link">
                 Max
               </td>
             </tr>
@@ -217,10 +199,7 @@ export const App = () => (
               <td data-cy="ProductName">Bread</td>
               <td data-cy="ProductCategory">🍞 - Grocery</td>
 
-              <td
-                data-cy="ProductUser"
-                className="has-text-danger"
-              >
+              <td data-cy="ProductUser" className="has-text-danger">
                 Anna
               </td>
             </tr>
@@ -233,10 +212,7 @@ export const App = () => (
               <td data-cy="ProductName">iPhone</td>
               <td data-cy="ProductCategory">💻 - Electronics</td>
 
-              <td
-                data-cy="ProductUser"
-                className="has-text-link"
-              >
+              <td data-cy="ProductUser" className="has-text-link">
                 Roma
               </td>
             </tr>
